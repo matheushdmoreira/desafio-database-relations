c7e16c5f1ee87048fe27f21d7563181332621722
Matheus Moreira <matheustjp111@gmail.com>
2020-05-27 18:58:49 -0300

    Entrega de desafio

--- src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
+++ src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
@@ -11,12 +11,26 @@ class OrdersRepository implements IOrdersRepository {
     this.ormRepository = getRepository(Order);
   }
 
-  public async create({ customer, products }: ICreateOrderDTO): Promise<Order> {
-    // TODO
+  public async create({
+    customer,
+    products,
+  }: ICreateOrderDTO): Promise<Order | undefined> {
+    const order = await this.ormRepository.create({
+      customer_id: customer.id,
+      order_products: products,
+    });
+
+    await this.ormRepository.save(order);
+
+    const RefreshOrder = this.findById(order.id);
+
+    return RefreshOrder;
   }
 
   public async findById(id: string): Promise<Order | undefined> {
-    // TODO
+    const findOrder = await this.ormRepository.findOne({ where: { id } });
+
+    return findOrder;
   }
 }
 
--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -21,21 +21,48 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const product = await this.ormRepository.findOne({
+      where: {
+        name,
+      },
+    });
+
+    return product;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const findProducts = await this.ormRepository.findByIds(products);
+
+    return findProducts;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    products.map(product => {
+      this.ormRepository
+        .createQueryBuilder()
+        .update(Product)
+        .set({ quantity: () => `quantity - ${product.quantity}` })
+        .where('id = :id', { id: `${product.id}` })
+        .execute();
+    });
+
+    const newProducts = await this.findAllById(products);
+
+    return newProducts;
   }
 }
 
